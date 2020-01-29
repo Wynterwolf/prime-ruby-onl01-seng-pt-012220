cbe5d6d98d88e36c82eea005cee256049056c8fc
Samantha Jordan <shinyguns@gmail.com>
2020-01-28 18:02:04 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number) #define method
+    start = 2 #starting at 2
+    if number > 1 #if larger than 1
+        range = (start..number-1).to_a
+        range.none? do |num_to_test|
+            number % num_to_test == 0
+        end
+    else
+        false 
+    end
+end
